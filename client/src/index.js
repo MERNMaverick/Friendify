982c2ba011f828a07c000fef6f596436794c46be
MERNMaverick <157880954+MERNMaverick@users.noreply.github.com>
2024-05-16 15:50:32 +0200

    added both folders

---
+++ client/src/index.js
@@ -0,0 +1,18 @@
+// index.js
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App";
+import store, { persistor } from "./store/configureStore";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
