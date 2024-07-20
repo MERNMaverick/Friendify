2691fa14675e4ab965b7de242f9424099566ad05
MERNMaverick <157880954+MERNMaverick@users.noreply.github.com>
2024-07-20 14:21:17 +0200

    Updated ESLint and prettier

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,22 +1,24 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginPrettier from "eslint-plugin-prettier";
-import configPrettier from "eslint-config-prettier";
+const { defineConfig } = require('eslint');
+const pluginJs = require('@eslint/js');
+const pluginPrettier = require('eslint-plugin-prettier');
 
-export default [
-  {
-    languageOptions: { globals: globals.node }, // Use globals.node for Node.js
+module.exports = defineConfig({
+  languageOptions: {
+    globals: {
+      ...require('globals').node,
+    },
     parserOptions: {
       ecmaVersion: 2021,
-      sourceType: "module",
+      sourceType: 'module',
     },
   },
-  pluginJs.configs.recommended,
-  {
-    plugins: [pluginPrettier],
-    extends: ["eslint:recommended", "plugin:prettier/recommended", "prettier"],
-    rules: {
-      "prettier/prettier": "error", // Show Prettier issues as ESLint errors
-    },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  plugins: [pluginPrettier],
+  rules: {
+    'prettier/prettier': 'error',
   },
-];
+});
