ed8e2ac1ba984f400083ba3c5d410964b849f855
MERNMaverick <157880954+MERNMaverick@users.noreply.github.com>
2024-07-20 11:29:54 +0200

    Added mongoose and connected successfully to database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 // Packages used are defined here
 import express from "express";
 import dotenv from "dotenv";
+import db from "mongoose";
 
 // Load environment variables from .env file
 dotenv.config();
@@ -13,6 +14,13 @@ app.use(express.json());
 
 // Server configuration
 const PORT = process.env.PORT || 5000;
+
+// Database connection configuration
+db.connect(process.env.MONGODB_URI).then(() =>
+  console.log("Connected to MongoDB")
+);
+
+// TODO: handle cases in which the database connection fails
 
 // Server startup configuration
 app.listen(PORT, () => {
