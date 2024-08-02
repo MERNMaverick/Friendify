14a7094975a7d2cdb013fe3615121040828815a7
MERNMaverick <157880954+MERNMaverick@users.noreply.github.com>
2024-08-02 22:00:24 +0200

    Added packages for the backend such as cors, multer for uploading files, helmet and other packages for file upload locally. Also, included the middlewares

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,28 +1,58 @@
 // Packages used are defined here
 import express from 'express'
 import dotenv from 'dotenv'
-import db from 'mongoose'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import multer from 'multer'
+
+// CONFIGURATION
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// INITIALIZING EXPRESS APP
+const app = express()
+
+// MIDDLEWARE
+app.use(express.json());
+app.use("/assets", express.static(path.join(__dirname, 'public/assets')))
+app.use(cors());
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+app.use(morgan('common'))
+
 
 // Load environment variables from .env file
 dotenv.config()
 
-// Initialize Express app
-const app = express()
 
-// Middleware to parse JSON request bodies
-app.use(express.json())
+// File Storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/assets')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname)
+  },
+}
+);
+
+const upload = multer({ storage})
 
 // Server configuration
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 // Database connection configuration
-db.connect(process.env.MONGODB_URI).then(() =>
-  console.log('Connected to MongoDB')
-)
-
-// TODO: handle cases in which the database connection fails
-
-// Server startup configuration
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.log(`Connection failed: ${error.message}`)
+  })
