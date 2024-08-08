e31cd84ca404bf91fd366ffbcb8fa8c00cb9eade
MERNMaverick <157880954+MERNMaverick@users.noreply.github.com>
2024-08-09 01:50:40 +0200

    update to cors, so frontend can connect

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,10 +22,18 @@ const __dirname = path.dirname(__filename);
 // INITIALIZING EXPRESS APP
 const app = express()
 
+// CORS OPTIONS
+const corsOptions = {
+  origin: 'https://friendify-ixjb.onrender.com', // Frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true 
+};
+
 // MIDDLEWARE
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
-app.use(cors());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common')); // Ensure Morgan is used
