5ec1984879c673893ea4395de8e1606af27c6058
MERNMaverick <157880954+MERNMaverick@users.noreply.github.com>
2024-08-10 22:22:42 +0200

    Update UserImage.jsx
    Removing http:// before the https://

--- client/src/components/UserImage.jsx
+++ client/src/components/UserImage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`http://https://friendify-backend-api.onrender.com/assets/${image}`}
+        src={`https://friendify-backend-api.onrender.com/assets/${image}`}
       />
     </Box>
   );
