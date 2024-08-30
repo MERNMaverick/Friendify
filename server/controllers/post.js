b12b40888eabc5956e5e4aca865becbf94f39bc3
MERNMaverick <157880954+MERNMaverick@users.noreply.github.com>
2024-08-30 14:38:18 +0200

    added search for posts and users controller

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -55,6 +55,33 @@ export const getUserPosts = async (req, res) => {
   }
 };
 
+/* SEARCH FOR POSTS */
+export const searchUsersAndPosts = async (req, res) => {
+  try {
+    const { q } = req.query;
+    
+    // SEARCH FOR USERS
+    const users = await User.find({
+      $or: [
+        { firstName: { $regex: q, $options: "i" } },
+        { lastName: { $regex: q, $options: "i" } },
+      ]
+    }).select("-password");
+
+    // SEARCH FOR POSTS
+    const posts = await Post.find({
+      $or: [
+        { description: { $regex: q, $options: "i" } },
+        { "comments.comment": { $regex: q, $options: "i" } }
+      ]
+    }).populate("userId", "firstName lastName picturePath");
+
+    res.json({ users, posts });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 /* ADD COMMENT */
 export const addComment = async (req, res) => {
   try {
